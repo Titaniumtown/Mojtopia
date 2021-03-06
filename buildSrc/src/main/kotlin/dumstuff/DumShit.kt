7fede0e28a9eae3236c1f22dd57930485abb57aa
Simon Gardling <titaniumtown@gmail.com>
2021-03-06 15:52:30 -0500

    update snapshot stuff

--- buildSrc/src/main/kotlin/dumstuff/DumShit.kt
+++ buildSrc/src/main/kotlin/dumstuff/DumShit.kt
@@ -26,14 +26,30 @@ fun dumShitTasks(project: Project): List<Task> {
     val modDecomp = workFolder.resolve("modDecomp")
     val patched = workFolder.resolve("patched")
     val paperWorkDir = project.projectDir.resolve("work/Paper/work")
-    val serverUrl = "https://launcher.mojang.com/v1/objects/99c3a9744719d0d401af63bb684cf1eb5231a75c/server.jar"
-    val serverMappingUrl = "https://launcher.mojang.com/v1/objects/66ebacdeccfdf8f6439f7a90234fc76e8ef5c5a6/server.txt"
-    val minecraftVersion = "21w07a"
-    /*
-    val serverUrl = "https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar"
-    val serverMappingUrl = "https://launcher.mojang.com/v1/objects/41285beda6d251d190f2bf33beadd4fee187df7a/server.txt"
-    val minecraftVersion = "1.16.5"
-    */
+    var serverMappingUrl = ""
+    var serverUrl = ""
+
+    val minecraftVersion = "21w08a"
+
+    if (minecraftVersion == "20w49a") {
+        serverUrl = "https://launcher.mojang.com/v1/objects/2fc0afe1fd31ca872761efbd2a7f635db234b359/server.jar"
+        serverMappingUrl = "https://launcher.mojang.com/v1/objects/0b30deba62ef6c2064dfd12f4f46b9d6388d9c8c/server.txt"
+    } else if (minecraftVersion == "1.16.5") {
+        serverUrl = "https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar"
+        serverMappingUrl = "https://launcher.mojang.com/v1/objects/41285beda6d251d190f2bf33beadd4fee187df7a/server.txt"
+    } else if (minecraftVersion == "21w06a") {
+        serverUrl = "https://launcher.mojang.com/v1/objects/6290ba4b475fca4a74de990c7fd8eccffd9654dd/server.jar"
+        serverMappingUrl = "https://launcher.mojang.com/v1/objects/c4e373406d2166580c33b075c2d05d9d2fb18d43/server.txt"
+    } else if (minecraftVersion == "21w07a") {
+        serverUrl = "https://launcher.mojang.com/v1/objects/99c3a9744719d0d401af63bb684cf1eb5231a75c/server.jar"
+        serverMappingUrl = "https://launcher.mojang.com/v1/objects/66ebacdeccfdf8f6439f7a90234fc76e8ef5c5a6/server.txt"
+    } else if (minecraftVersion == "21w08a") {
+        serverUrl = "https://launcher.mojang.com/v1/objects/d5e31633d884e190e046b8645f802541bec2a5e9/server.jar"
+        serverMappingUrl = "https://launcher.mojang.com/v1/objects/64b781c30f7fa920c721838f53510861ca3f8d4a/server.txt"
+    } else {
+        System.out.println("ERROR: minecraftVersion is invalid")
+        System.exit(-1)
+    }
 
     val decompileMod: Task by project.tasks.creating {
         group = taskGroupPrivate
